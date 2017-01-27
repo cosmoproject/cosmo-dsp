0f34f9e96ab43e6bc7ff105c962399794c4aa8bf
ketchupok <ahah@gmx.net>
2017-01-27 14:02:40 +0100

    added comments in UDOsToTable.py

--- DSP-Library/Scripts/UDOsToTable.py
+++ DSP-Library/Scripts/UDOsToTable.py
@@ -4,14 +4,13 @@ from os.path import isfile, join
 import string, inspect
 from tabulate import tabulate
 
-#mypath = '/home/iwkah/Documents/Programming/cosmo-dsp/DSP-Library/Effects'
-#mypath = '/Users/elk/Programming/cosmoProject/cosmo-dsp/DSP-Library/Effects'
+# read all file names in directory
 fileDir = os.path.dirname(__file__)
 mypath = os.path.join(fileDir, '../Effects/')
 onlyfiles = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
 # print onlyfiles
 
-
+# read Arguments given in UDOs
 udos = []
 for csd in onlyfiles:
     arguments = []
@@ -19,9 +18,6 @@ for csd in onlyfiles:
     filename = join(mypath + '/' + str(csd))
     with open(filename) as UDO_file:
         for line in UDO_file:
-            # if 'Arguments' in line:
-            #     argTokens = line.split(',')
-            #     print argTokens
             if 'Arguments' in line:
                 colPos = line.find(':')
                 if colPos is not -1:
@@ -30,21 +26,19 @@ for csd in onlyfiles:
     udos.append(arguments)
 print udos
 
+# make LATEX table, used in NIME 2017 paper
 head = ['UDO', 'Arguments']
-#print tabulate(udos, headers = head,  tablefmt = 'latex')
 latexTable = tabulate(udos, headers = head,  tablefmt = 'latex')
 latexTable = latexTable.replace(',', ' \par')
 print latexTable
 
+# make Markup table
 markupTable = tabulate(udos, headers = head, tablefmt = 'pipe')
 print markupTable
 
+# export Markup table into README.me file
 filename = os.path.join(fileDir, '../Effects/README.md')
 with open(filename, 'w+') as readme_file:
     readme_file.write('## List of supported effect UDOs: \n')
     readme_file.write(markupTable)
     readme_file.close
-
-
-# x = ['o', 'arg'],['x', '14, ds, 123, 34'], ['y', 'asd, \par asd \par, ad, wer']
-# print tabulate(x,headers = 'firstrow', tablefmt = 'latex')
