c1b5bb6fa71afd973a214e8dba0a15cfe9312f47
Gytha Ogg <gythaoggscat@gmail.com>
2017-05-01 17:10:04 +0200

    dummy ui and a script to collect effects and arguments

--- COSMO-Patcher/CosmoPatcherUI/webapp/scripts/cosmo.js
+++ COSMO-Patcher/CosmoPatcherUI/webapp/scripts/cosmo.js
@@ -3,25 +3,20 @@
  */
 
 var app = angular.module('cosmoApp', []);
-
+var prefix= 'pot'
 app.controller('cosmoCtrl', function($scope, $http) {
     $http.get('webapp/scripts/cosmo-controls.json').then(function(data) {
 	$scope.cosmoCtrls = angular.fromJson(data.data);//Object.values(data.data); // data.data;
     });
     $scope.orderProp = 'ctrlId';
-    $scope.maxControls=8;
-    $scope.numControls=0;
+    $scope.placeholders = [0, 1, 2, 3, 4, 5, 6, 7]
     $scope.updateControls = function(){
-	if ($scope.numControls < $scope.maxControls){
-	    $scope.numControls ++;
-	    console.log('added 1 - ' + $scope.numControls);
-	    return true;
-	}
-	console.log($scope.numControls);
-	return false;
+	console.log('Loaded: ');
+	return true;
     }
-    
+    $scope.out_json = {};
 });
+
 function allowDrop(ev) {
     ev.dataTransfer.dropEffect = 'move';
     ev.preventDefault();
@@ -33,23 +28,38 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
+    console.log(ev.target.className);
+    if (ev.target.className.split(' ')[0] != 'placeholder')
+	return;
     var data = ev.dataTransfer.getData("draggedId");
     var nodeCopy = document.getElementById(data).cloneNode(true);
-    nodeCopy.id = "selected"+data; /* Need to make this unique */
+    console.log(ev.target.id)
+    nodeCopy.id = prefix+ev.target.id;
     scope = getControllerScope();
-    if (scope.updateControls())
+    if (scope.updateControls()){
 	ev.target.append(nodeCopy);
+    }
+    if (!is_warehouse_element(data)){
+	var dataNode = document.getElementById(data);
+	dataNode.parentNode.removeChild(dataNode);
+    }
+}
+
+
+function is_warehouse_element(id) {
+    if (id.substring(0,4) != 'ctrl')
+	return false;
+    else
+	return true;
 }
 
 function trash(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("draggedId");
-    if (data.substring(0,4) != 'ctrl'){
+    if (!is_warehouse_element(data)){
 	var dataNode = document.getElementById(data);
 	scope = getControllerScope();
-	scope.numControls = scope.numControls - 1;
 	dataNode.parentNode.removeChild(dataNode);
-	console.log('removed 1 - ' + scope.numControls);
     }else{
 	console.log('not removing, control object');
     }
@@ -58,6 +68,6 @@ function trash(ev) {
 function getControllerScope() {
     var appElement = document.querySelector('[ng-app=cosmoApp]');
     var appScope = angular.element(appElement).scope();
-    //var controllerScope = appScope.$$childHead;
+    var controllerScope = appScope.$$childHead;
     return appScope;
 }
