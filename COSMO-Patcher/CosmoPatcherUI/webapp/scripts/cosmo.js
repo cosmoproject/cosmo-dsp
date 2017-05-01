0e8eb894815a162ff3df73d7c73c0e7e61f5a2df
Gytha Ogg <gythaoggscat@gmail.com>
2017-05-02 01:51:39 +0200

    before mapping effects

--- COSMO-Patcher/CosmoPatcherUI/webapp/scripts/cosmo.js
+++ COSMO-Patcher/CosmoPatcherUI/webapp/scripts/cosmo.js
@@ -2,7 +2,12 @@
  *cosmo.js
  */
 
-var app = angular.module('cosmoApp', []);
+var app = angular.module('cosmoApp',  ['ng-sortable']);
+app.constant('ngSortableConfig',
+	     {onEnd: function() {
+		 console.log('default onEnd()');
+	     }})
+//    .constant('ngSortableConfig'
 var prefix= 'pot'
 app.controller('cosmoCtrl', function($scope, $http, $timeout) {
     $http.get('webapp/scripts/cosmo-controls.json').then(function(data) {
@@ -17,8 +22,13 @@ app.controller('cosmoCtrl', function($scope, $http, $timeout) {
     $scope.out_json = {};
     $scope.selection = -1;
     $scope.selected_effect = -1;
+    $scope.arguments = [];    
     $scope.select_controller = function(element_id){
 	console.log("selecting "+ element_id);
+	if (element_id == -1){
+	    $scope.arguments = [];
+	    $scope.selected_effect = -1;
+	}
 	$scope.selection = element_id;
 	$timeout(); //.$scope.$apply();
     }
@@ -84,3 +94,58 @@ function getControllerScope() {
     var controllerScope = appScope.$$childHead;
     return appScope;
 }
+
+/* 
+angular.module('todoApp', ['ng-sortable'])
+    .constant('ngSortableConfig', {onEnd: function() {
+	console.log('default onEnd()');
+    }})
+    .controller('TodoController', ['$scope', function ($scope) {
+	$scope.todos = [
+	    {text: 'learn angular', done: true},
+	    {text: 'build an angular app', done: false}
+	];
+
+	$scope.addTodo = function () {
+	    $scope.todos.push({text: $scope.todoText, done: false});
+	    $scope.todoText = '';
+	};
+
+	$scope.remaining = function () {
+	    var count = 0;
+	    angular.forEach($scope.todos, function (todo) {
+		count += todo.done ? 0 : 1;
+	    });
+	    return count;
+	};
+
+	$scope.archive = function () {
+	    var oldTodos = $scope.todos;
+	    $scope.todos = [];
+	    angular.forEach(oldTodos, function (todo) {
+		if (!todo.done) $scope.todos.push(todo);
+	    });
+	};
+    }])
+    .controller('TodoControllerNext', ['$scope', function ($scope) {
+	$scope.todos = [
+	    {text: 'learn Sortable', done: true},
+	    {text: 'use ng-sortable', done: false},
+	    {text: 'Enjoy', done: false}
+	];
+
+	$scope.remaining = function () {
+	    var count = 0;
+	    angular.forEach($scope.todos, function (todo) {
+		count += todo.done ? 0 : 1;
+	    });
+	    return count;
+	};
+
+	$scope.sortableConfig = { group: 'todo', animation: 150 };
+	'Start End Add Update Remove Sort'.split(' ').forEach(function (name) {
+	    $scope.sortableConfig['on' + name] = console.log.bind(console, name);
+	});
+    }]);
+})();
+*/
