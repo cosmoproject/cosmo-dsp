330e27f14aa7e3f6ba94a82c8bba3cc87b38aec4
Gytha Ogg <gythaoggscat@gmail.com>
2017-05-02 14:12:55 +0200

    printing the pid of the webserver to quit

--- COSMO-Patcher/CosmoPatcherUI/start.sh
+++ COSMO-Patcher/CosmoPatcherUI/start.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 # update the list of effects and arguments
 python effects.py
+echo "effects collected, updated"
 # start a web server
 python -m SimpleHTTPServer 8888 &
+echo "web server started as a background process"
+# save the webserver pid
+SIMPLE_SERVER_PID=$!
+echo "web server process id: "$SIMPLE_SERVER_PID
 # open the design page
 xdg-open http://localhost:8888
