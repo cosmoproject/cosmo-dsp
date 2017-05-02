6050b97741a48f207ba76f18bcbe792616a973b4
Gytha Ogg <gythaoggscat@gmail.com>
2017-05-02 16:58:22 +0200

    sorting function in case necessary

--- COSMO-Patcher/CosmoPatcherUI/webapp/scripts/cosmo.js
+++ COSMO-Patcher/CosmoPatcherUI/webapp/scripts/cosmo.js
@@ -164,3 +164,7 @@ function getControllerScope() {
     var controllerScope = appScope.$$childHead;
     return appScope;
 }
+
+function sortObject(o) {
+    return Object.keys(o).sort().reduce((r, k) => (r[k] = o[k], r), {});
+}
