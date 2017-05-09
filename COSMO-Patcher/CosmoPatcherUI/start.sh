42580ec50db84e5d0ddf5b1527d175b6a6d7085e
Gytha Ogg <gythaoggscat@gmail.com>
2017-05-09 17:31:44 +0200

    reoved digital channels

--- COSMO-Patcher/CosmoPatcherUI/start.sh
+++ COSMO-Patcher/CosmoPatcherUI/start.sh
@@ -3,10 +3,10 @@
 python effects.py
 echo "effects collected, updated"
 # start a web server
-python -m SimpleHTTPServer 8888 &
+#python -m SimpleHTTPServer 8888 &
 # CGI webserver if we also want to run python functions
 # from the site
-#  python -m CGIHTTPServer 8888
+python -m CGIHTTPServer 8888
 echo "web server started as a background process"
 # save the webserver pid
 SIMPLE_SERVER_PID=$!
