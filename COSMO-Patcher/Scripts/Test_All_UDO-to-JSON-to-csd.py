95c09bc5ce1ce22ddd31a8a7f633410ffcb88204
ketchupok <ahah@gmx.net>
2018-10-12 13:38:56 +0200

    towards python 3 compability

--- COSMO-Patcher/Scripts/Test_All_UDO-to-JSON-to-csd.py
+++ COSMO-Patcher/Scripts/Test_All_UDO-to-JSON-to-csd.py
@@ -19,7 +19,7 @@ from CosmoGraph.CosmoPatcherGraph import CosmoPatcherGraph
 
 # Read all Csound UDOS in Effect Library
 fileDir = os.path.dirname(__file__)
-mypath = os.path.join(fileDir, '../DSP-Library/Effects/')
+mypath = os.path.join(fileDir, '../../DSP-Library/Effects/')
 onlyfiles = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
 # print onlyfiles
 
@@ -27,9 +27,9 @@ onlyfiles = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
 def _open_COSMO_UDO_read_args(udoName):
     defaultValues = []
     arguments = []
-    print '..reading csd ' + str(udoName)
+    print('..reading csd ' + str(udoName))
     fileDir = os.path.dirname(__file__)
-    filename = os.path.join(fileDir, '../DSP-Library/Effects/'
+    filename = os.path.join(fileDir, '../../DSP-Library/Effects/'
                             + str(udoName))
     with open(filename) as UDO_file:
         for line in UDO_file:
@@ -53,10 +53,10 @@ def _open_COSMO_UDO_read_args(udoName):
 
 def write_json(fileName, content_json):
     fileDir = os.path.dirname(__file__)
-    mypath = os.path.join(fileDir, '../TestAllUdos/')
+    mypath = os.path.join(fileDir, '../../TestAllUdos/')
     if not os.path.exists(mypath):
             os.makedirs(mypath)
-    filename = os.path.join(fileDir, '../TestAllUdos/' + fileName[:-4] + '.json')
+    filename = os.path.join(fileDir, '../../TestAllUdos/' + fileName[:-4] + '.json')
     with open(filename, 'w+') as json_file:
         json_file.write(content_json)
         json_file.close
@@ -71,7 +71,7 @@ for file_name in onlyfiles:
     cc_dict = {}
     udo_inputs = _open_COSMO_UDO_read_args(file_name)
     # udo_inputs['argNames']
-    print udo_inputs
+    print(udo_inputs)
     for idx, arg in enumerate(udo_inputs):
         cc_num = 'CC%s_CH%s' % (start_cc_num+idx, 1)
         udo_dict = {}
@@ -83,24 +83,24 @@ for file_name in onlyfiles:
     write_json(file_name, json.dumps(main_dict, indent=2))
 
 ## --- make csd's from JSON ----
-mypathT = os.path.join(fileDir, '../TestAllUdos/')
+mypathT = os.path.join(fileDir, '../../TestAllUdos/')
 generated_json_f = sorted([f for f in listdir(mypathT) if isfile(join(mypathT, f))])
 for file_name in generated_json_f:
     C_set = CosmoPatcherGraph()
     fileDir = os.path.dirname(__file__)
-    input_filename = os.path.join(fileDir, '../TestAllUdos/' + file_name)
-    print 'Load Json file ' + input_filename
+    input_filename = os.path.join(fileDir, '../../TestAllUdos/' + file_name)
+    print('Load Json file ' + input_filename)
     C_set.read_settings_json(input_filename)
 
-    print 'Convert Json to Graph / csd structure.'
+    print('Convert Json to Graph / csd structure.')
     C_set.cosmo_settings_to_graph()
 
     # print 'All nodes and edges'
     # print C_set.nodes()
     # print C_set.edges(data = True)
 
-    print 'Generate Csound File'
+    print('Generate Csound File')
     C_set.generate_csound_code_from_graph()
     output_filename = mypathT + file_name + '.csd'
-    print 'Write Csound file .. ' + output_filename
+    print('Write Csound file .. ' + output_filename)
     C_set.write_csd(output_filename)
