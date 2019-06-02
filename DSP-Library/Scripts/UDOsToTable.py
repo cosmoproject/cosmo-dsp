6a923572b420f78a5b230cf1b36a80ebda7a8dea
ketchupok <ahah@gmx.net>
2019-06-02 13:20:59 -0300

    updated for python 3 compability, and new location in lib

--- DSP-Library/Scripts/UDOsToTable.py
+++ DSP-Library/Scripts/UDOsToTable.py
@@ -1,3 +1,9 @@
+# This file updates the Readme.md file with the list of UDOS.
+# Terminal:
+# pip install tabulate
+# python UDOsToTable.py
+# Alex Hofmann (2017-19)
+
 import os
 from os import listdir
 from os.path import isfile, join
@@ -6,14 +12,14 @@ from tabulate import tabulate
 
 # read all file names in directory
 fileDir = os.path.dirname(__file__)
-mypath = os.path.join(fileDir, '../../DSP-Library/Effects/')
+mypath = os.path.join(fileDir, '../Effects/')
 onlyfiles = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
 # print onlyfiles
 
 # read Arguments given in UDOs
 udos = []
 for udo in onlyfiles:
-    if udo[-4:] == '.udo':                
+    if udo[-4:] == '.udo':
         arguments = []
         arguments.append(udo)
         filename = join(mypath + '/' + str(udo))
@@ -25,20 +31,20 @@ for udo in onlyfiles:
                         b = line[colPos+1:]
                         arguments.append(b)
         udos.append(arguments)
-print udos
+print(udos)
 
 # make LATEX table, used in NIME 2017 paper
 head = ['UDO', 'Arguments']
 latexTable = tabulate(udos, headers = head,  tablefmt = 'latex')
 latexTable = latexTable.replace(',', ' \par')
-print latexTable
+print(latexTable)
 
 # make Markup table
 markupTable = tabulate(udos, headers = head, tablefmt = 'pipe')
-print markupTable
+print(markupTable)
 
 # export Markup table into README.me file
-filename = os.path.join(fileDir, '../../DSP-Library/README.md')
+filename = os.path.join(fileDir, 'README.md')
 with open(filename, 'w+') as readme_file:
     readme_file.write('## List of effects: \n')
     readme_file.write(markupTable)
